82671f2428bd3e02959f25ebd63f8a341873c42f
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2025-03-16 01:44:50 +0700

    add text responsive

--- src/pages/home/MinProjectWrapper.tsx
+++ src/pages/home/MinProjectWrapper.tsx
@@ -12,7 +12,7 @@ interface MinProjectWrapperProps {
 }
 export default function MinProjectWrapper({ project, handleOnScreen }: MinProjectWrapperProps) {
 	const inViewRef = useRef<HTMLDivElement>(null);
-	const isInView = useInView(inViewRef, { once: false, amount: 0.9 });
+	const isInView = useInView(inViewRef, { once: false, amount: 0.8 });
 	// Update onScreen state
 	useEffect(() => {
 		if (isInView) {
@@ -29,7 +29,7 @@ export default function MinProjectWrapper({ project, handleOnScreen }: MinProjec
 		<div
 			ref={inViewRef}
 			id={"project-" + project.id}
-			className="w-full min-h-dvh lg:px-56 lg:py-32 flex justify-center items-center"
+			className="w-full min-h-dvh px-18 sm:px-28 md:px-36 lg:px-42 xl:px-46 lg:py-32 flex justify-center items-center"
 		>
 			<motion.section
 				ref={cursorRef}
@@ -38,10 +38,10 @@ export default function MinProjectWrapper({ project, handleOnScreen }: MinProjec
 				transition={{ duration: 0.3, ease: "easeIn" }}
 				className="relative z-10"
 			>
-				<motion.h3 className="font-ballet text-[10rem] leading-none drop-shadow-custom">
+				<motion.h3 className="w-fit font-ballet text-[5rem] sm:text-[7rem] md:text-[9rem] lg:text-[11rem] leading-none drop-shadow-custom">
 					<ShinyText text={project.title} speed={4} />
 				</motion.h3>
-				<p className="font-bodoni text-[2rem] leading-tight font-semibold dark:font-normal text-stone-600 dark:text-stone-300 drop-shadow-custom">
+				<p className="font-bodoni text-[1.2rem] sm:text-[1.5rem] md:text-[1.7rem] lg:text-[2rem] leading-tight font-semibold dark:font-normal text-stone-600 dark:text-stone-300 drop-shadow-custom">
 					<BlurText text={project.tagline} delay={150} />
 				</p>
 			</motion.section>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -13,7 +13,6 @@ const fetchMinProjects = async () => {
 	const minProjects = await fetch(import.meta.env.VITE_BASE_API_URL + "/api/project")
 		.then((res) => res.json())
 		.then((data) => data.data);
-
 	return minProjects;
 };
 
@@ -24,6 +23,8 @@ export default function Home() {
 	});
 
 	const { theme, toggleTheme } = useTheme();
+
+	const { scrollYProgress } = useScroll();
 
 	const [onViewProject, setOnViewProject] = useState<number>(NaN);
 	const [onScreen, setOnScreen] = useState<boolean[]>(() => (data ? Array(data.length).fill(false) : []));
@@ -43,22 +44,71 @@ export default function Home() {
 		});
 	}, []);
 
-	const { scrollYProgress } = useScroll();
+	const [screenBreakpoint, setScreenBreakpoint] = useState("xl");
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth >= 1280) {
+				setScreenBreakpoint("xl");
+			} else if (window.innerWidth >= 1024) {
+				setScreenBreakpoint("lg");
+			} else if (window.innerWidth >= 768) {
+				setScreenBreakpoint("md");
+			} else if (window.innerWidth >= 640) {
+				setScreenBreakpoint("sm");
+			} else {
+				setScreenBreakpoint("xs");
+			}
+		};
+		handleResize();
+
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	});
+	const horizontalWide = () => {
+		switch (screenBreakpoint) {
+			case "xl":
+				return 80;
+			case "lg":
+				return 75;
+			case "md":
+				return 70;
+			case "sm":
+				return 65;
+			default:
+				return 60;
+		}
+	};
+	const verticalWide = () => {
+		switch (screenBreakpoint) {
+			case "xl":
+				return 60;
+			case "lg":
+				return 55;
+			case "md":
+				return 45;
+			case "sm":
+				return 40;
+			default:
+				return 35;
+		}
+	};
 
 	return (
 		<>
 			<PreloadPage deps={[!isLoading]} />
 
 			<CustomFrame
-				wide={80}
-				verticalWide={60}
+				horizontalWide={horizontalWide()}
+				verticalWide={verticalWide()}
 				focusCrosshair
 				frameClassName="bg-black/6 dark:bg-white/6 backdrop-blur"
 				focusedCrosshair={isNaN(onViewProject)}
 			>
 				<CustomFrameTop>
 					<div className="w-full h-full px-4 flex justify-between items-center">
-						<span className="basis-1/3 font-kode text-6xl text-start text-stone-600 dark:text-zinc-300">
+						<span className="basis-1/3 font-kode text-4xl sm:text-5xl lg:text-6xl text-start text-stone-600 dark:text-zinc-300">
 							<DecryptedText
 								key={onViewProject}
 								text={Number(onViewProject + 1)
@@ -67,8 +117,10 @@ export default function Home() {
 								animateOn="view"
 							/>
 						</span>
-						<h1 className="basis-1/3 font-doto font-semibold dark:font-normal text-lg text-center">ADE / PORTFOLIO</h1>
-						<span className="basis-1/3 font-doto font-bold dark:font-semibold text-2xl text-end">
+						<h1 className="basis-1/3 font-doto font-semibold dark:font-normal text-sm sm:text-base lg:text-lg text-center">
+							ADE / PORTFOLIO
+						</h1>
+						<span className="basis-1/3 font-doto font-bold dark:font-semibold text-lg sm:text-xl lg:text-2xl text-end">
 							<DecryptedText
 								key={onViewProject}
 								text={Array.isArray(data) ? (!isNaN(onViewProject) ? data[onViewProject].title : "Null") : "Null"}
@@ -81,7 +133,7 @@ export default function Home() {
 				</CustomFrameTop>
 				<CustomFrameBottom>
 					<div className="w-full h-full px-6 flex justify-between items-center">
-						<span className="font-doto font-semibold dark:font-normal text-lg text-start">
+						<span className="font-doto font-semibold dark:font-normal text-sm sm:text-base lg:text-lg text-start">
 							<DecryptedText
 								key={onViewProject}
 								text={
@@ -99,7 +151,7 @@ export default function Home() {
 						<button
 							type="button"
 							disabled={isNaN(onViewProject)}
-							className="border border-stone-400 dark:border-zinc-500 font-doto font-semibold dark:font-normal text-lg text-center hover:bg-stone-400 dark:hover:bg-zinc-500 active:bg-stone-600 dark:active:bg-zinc-700 focus:outline-2 focus:outline-offset-2 focus:outline-stone-700 dark:focus:outline-zinc-300 transition-colors duration-500 ease-initial cursor-pointer disabled:hover:bg-transparent disabled:active:bg-transparent disabled:focus:outline-none disabled:cursor-default disabled:opacity-30"
+							className="border border-stone-400 dark:border-zinc-500 font-doto font-semibold dark:font-normal text-sm sm:text-base lg:text-lg text-center hover:bg-stone-400 dark:hover:bg-zinc-500 active:bg-stone-600 dark:active:bg-zinc-700 focus:outline-2 focus:outline-offset-2 focus:outline-stone-700 dark:focus:outline-zinc-300 transition-colors duration-500 ease-initial cursor-pointer disabled:hover:bg-transparent disabled:active:bg-transparent disabled:focus:outline-none disabled:cursor-default disabled:opacity-30"
 						>
 							<DecryptedText text={"// Expand"} sequential parentClassName="px-3 py-1" />
 						</button>
@@ -110,19 +162,19 @@ export default function Home() {
 						<button
 							onClick={toggleTheme}
 							type="button"
-							className="size-8 aspect-square bg-stone-400/50 dark:bg-zinc-600/50 hover:bg-stone-500/50 dark:hover:bg-zinc-500/50 font-doto font-bold dark:font-normal text-sm text-center"
+							className="size-6 lg:size-8 aspect-square bg-stone-400/50 dark:bg-zinc-600/50 hover:bg-stone-500/50 dark:hover:bg-zinc-500/50 font-doto font-bold dark:font-normal text-xs lg:text-sm text-center"
 						>
 							{theme === "light" ? "L" : "D"}
 						</button>
 
 						<div className="flex flex-col items-center gap-2">
 							<span
-								className="min-h-[14rem] w-fit px-2 border border-stone-400 dark:border-zinc-600 font-doto text-sm font-bold dark:font-normal text-stone-800 dark:text-zinc-200 tracking-widest text-end"
+								className="min-h-[14rem] w-fit px-2 border border-stone-400 dark:border-zinc-600 font-doto text-xs lg:text-sm font-bold dark:font-normal text-stone-800 dark:text-zinc-200 tracking-widest text-end"
 								style={{ writingMode: "vertical-rl" }}
 							>
 								{"/".repeat(Math.ceil(scrollYProgress.get() * 20) + 1)}
 							</span>
-							<span className="font-kode text-xs text-stone-500 dark:text-zinc-400">
+							<span className="font-kode text-[10px] lg:text-xs text-stone-500 dark:text-zinc-400">
 								{Math.ceil(scrollYProgress.get() * 100)}%
 							</span>
 						</div>
@@ -131,7 +183,7 @@ export default function Home() {
 			</CustomFrame>
 
 			<ReactLenis root options={{ infinite: true, syncTouch: true, smoothWheel: true }}>
-				<main className="px-28">
+				<main>
 					{data &&
 						data.map((project: MinProject, i: number) => (
 							<MinProjectWrapper
