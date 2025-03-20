55367f991f739cc03ca62e9337c655622a955208
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2025-03-21 02:30:14 +0700

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 const env: "dev" | "prod" = import.meta.env.VITE_ENV || "dev";
 console.log(`Environment: ${env}`);
@@ -11,6 +12,9 @@ createRoot(document.getElementById("root")!).render(
 			<App />
 		</StrictMode>
 	) : (
-		<App />
+		<>
+			<App />
+			<Analytics />
+		</>
 	)
 );
