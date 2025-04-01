4dc98453ccebbca7e96db878fb587eea026f1e4a
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2025-04-01 19:23:35 +0700

    fix: change custom cursor to cursor hidden

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -1,4 +1,6 @@
-import { MinProject } from "../types/Project";
+export function CursorHidden() {
+	return null;
+}
 
 export function CursorDefault() {
 	const className =
@@ -14,26 +16,6 @@ export function CursorDefault() {
 			<div className={className} style={{ animationDuration: "4.5s", animationDirection: "reverse" }} />
 			<div className={className} style={{ animationDuration: "5s" }} />
 			<div className={className} style={{ animationDuration: "5.5s", animationDirection: "reverse" }} />
-		</div>
-	);
-}
-
-export function HoveringProjectCursor({ project }: { project: MinProject }) {
-	return (
-		<div
-			className="p-[1px] drop-shadow-cursor animate-shine"
-			style={{
-				backgroundImage:
-					"linear-gradient(120deg, rgba(255, 215, 0, 0.9) 45%, oklch(0.985 0 0 / 0.9) 55%, rgba(255, 215, 0, 0.9) 65%, rgba(255, 215, 0, 0.9) 70%, oklch(0.985 0 0 / 0.9) 75%, rgba(255, 215, 0, 0.9) 80%)",
-				backgroundSize: "200% 100%",
-				animationDuration: "3s"
-			}}
-		>
-			<img
-				src={project.image_thumbnail_url}
-				alt={project.title}
-				className="w-[calc(7vw+4vh+150px)] aspect-video object-cover object-center brightness-90 dark:brightness-80 bg-neutral-500"
-			/>
 		</div>
 	);
 }
--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,6 +1,6 @@
 import { CustomCursorProvider } from "./useCustomCursor";
-import { CursorDefault } from "../components/CustomCursor";
+import { CursorHidden } from "../components/CustomCursor";
 
 export default function ContextProvider({ children }: { children: React.ReactNode }) {
-	return <CustomCursorProvider DefaultCursor={CursorDefault}>{children}</CustomCursorProvider>;
+	return <CustomCursorProvider DefaultCursor={CursorHidden}>{children}</CustomCursorProvider>;
 }
