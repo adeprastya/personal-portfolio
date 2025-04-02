f7ddb4cf4fac855a2bbf562e1826475d89659a4a
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2025-04-03 00:57:06 +0700

    route transition minor animation change

--- src/components/shared/RouteTransition.tsx
+++ src/components/shared/RouteTransition.tsx
@@ -1,6 +1,7 @@
+import type { Variants } from "framer-motion";
 import { motion } from "framer-motion";
 
-const parentVars = {
+const parentVars: Variants = {
 	initial: {
 		backdropFilter: "blur(0px)"
 	},
@@ -13,17 +14,20 @@ const parentVars = {
 		transition: { delay: 0.4, duration: 0.4, ease: "linear" }
 	}
 };
-const childVars = {
+const childVars: Variants = {
 	initial: {
-		height: "0"
+		height: "100dvh",
+		width: "0"
 	},
 	animate: {
 		height: "100dvh",
-		transition: { delay: 0.4, duration: 0.4, ease: "easeIn" }
+		width: "100vw",
+		transition: { delay: 0.4, duration: 0.4, ease: "easeInOut" }
 	},
 	exit: {
 		height: "0",
-		transition: { duration: 0.4, ease: "easeOut" }
+		width: "100vw",
+		transition: { duration: 0.4, ease: "easeInOut" }
 	}
 };
 
@@ -34,21 +38,19 @@ export default function RouteTransition({ isInitial }: { isInitial: boolean }) {
 			initial={isInitial ? "animate" : "initial"}
 			animate="animate"
 			exit="exit"
-			className="fixed z-[999] top-0 left-0 w-screen h-screen"
+			className="fixed z-[999] top-0 left-0 w-screen h-screen flex items-center justify-center"
 		>
 			<motion.div
 				variants={childVars}
 				initial={isInitial ? "animate" : "initial"}
 				animate="animate"
 				exit="exit"
-				className="bg-stone-200 dark:bg-zinc-900 overflow-hidden"
+				className="w-screen h-screen bg-stone-200 dark:bg-zinc-900 overflow-hidden flex items-center justify-center"
 			>
-				<div className="w-screen h-screen flex items-center justify-center">
-					<div className="p-2 border-2 border-gold font-doto font-semibold text-5xl text-gold flex flex-col animate-pulse">
-						<span>POR</span>
-						<span>TFO</span>
-						<span>LIO</span>
-					</div>
+				<div className="p-2 border-2 border-gold font-doto font-semibold text-5xl text-gold flex flex-col animate-pulse">
+					<span>POR</span>
+					<span>TFO</span>
+					<span>LIO</span>
 				</div>
 			</motion.div>
 		</motion.div>
